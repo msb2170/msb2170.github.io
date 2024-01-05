2b6fed8fda720a1600e10ac2e13bd4f214ddb1d3
Matthew <msb2170@gmail.com>
2024-01-05 15:03:12 -0500

    v2 1.0

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
-const threeDots = document.getElementById('three-dots');
-const scrollUp = document.getElementById('scroll-up');
-const dateField = document.getElementById('date')
-const ul = document.querySelector('nav ul');
+const contentLinks = document.querySelectorAll(".content-link")
 
-const navLink = document.querySelectorAll('.nav-link');
-
-
-scrollUp.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
+contentLinks.forEach((link) => {
+    link.addEventListener("click", function(event) {
+        event.preventDefault()
+        const targetId = this.textContent
+        console.log(targetId)
+        const targetElem = document.getElementById(targetId)
+        if (targetElem) {
+            targetElem.scrollIntoView({ behavior: "smooth"})
+        }
     })
 })
-
-
-threeDots.addEventListener('click', () => {
-    ul.classList.toggle('show');
-});
-
-
-navLink.forEach((link) => 
-    link.addEventListener('click', function() {
-        ul.classList.remove('show');
-        
-    })
-);
-
-dateField.innerHTML = new Date().getFullYear()
